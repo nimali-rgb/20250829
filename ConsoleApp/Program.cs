0d8d07ba4db62b6ca625336e9ba6106763112368
Nima <nima010607@gmail.com>
2025-09-02 10:32:49 +0200

    clear out

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,2 @@
-﻿List<string> names = [];
+﻿
 
-Console.Write("Ange ett namn: ");
-names.Add(Console.ReadLine()!);
-
-Console.WriteLine("Namn i listan: ");
-foreach (var name in names);
-{
-    Console.WriteLine(names);
-}
-
-Console.WriteLine("Tryck på valfri tangent för att avsluta....");
-Console.ReadKey();
