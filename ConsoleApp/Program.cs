45a9ec8cbc49fc06279191e329cde75150280473
Nima <nima010607@gmail.com>
2025-09-02 10:33:50 +0200

    input out put exampels

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
 ﻿
 
+
+List<string> name_List = [];
+
+Console.WriteLine("Hej och välkommen. ");
+string answer = "";
+
+do
+{
+    Console.Write("ange ditt namn: ");
+
+    string input_name = Console.ReadLine()!;
+
+    if (!string.IsNullOrEmpty(input_name))
+    {
+        name_List.Add(input_name);
+    }
+
+    Console.Write("Vill du ange ett nytt namn? y/n:  ");
+    answer = Console.ReadLine()!;
+}
+while (answer == "y"); //med true i så blir den för evigt loop
+
+
+Console.Clear();
+
+
+Console.WriteLine("##NANMNLISTA##");
+
+foreach (string name in name_List)
+{
+    Console.WriteLine(name);
+}
+
+
+Console.ReadKey();
